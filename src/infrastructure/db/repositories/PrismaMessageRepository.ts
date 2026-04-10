c296cfc2c5239a46f01c19f1b2bcdaf9dea1509a
Ezagu <agustincalpedm@gmail.com>
2026-04-10 04:28:45 -0300

    order message

--- src/infrastructure/db/repositories/PrismaMessageRepository.ts
+++ src/infrastructure/db/repositories/PrismaMessageRepository.ts
@@ -1,9 +1,9 @@
-import type { CreateMessage, Message } from "../../../domain/entities/Message.js";
+import type { CreateMessageInput } from "../../../domain/entities/Message.js";
 import type { MessageRepository } from "../../../domain/repositories/MessageRepository.js";
 import { prisma } from "../prisma/prisma.js";
 
 export class PrismaMessageRepository implements MessageRepository {
-  create = async (message: CreateMessage) => {
+  create = async (message: CreateMessageInput) => {
     return await prisma.message.create({
       data: message
     })
@@ -11,7 +11,10 @@ export class PrismaMessageRepository implements MessageRepository {
 
   findByChat = async (chatId: string) => {
     return await prisma.message.findMany({
-      where: {chatId}
+      where: {chatId},
+      orderBy: {
+        createdAt: 'desc'
+      }
     })
   };
 }
